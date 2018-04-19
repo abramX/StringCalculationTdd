c48dbf7cfeb4fb957ce93e4901de674821b2f0bf
Camara Ibrahima <camara@eurisnet.it>
2018-04-19 17:19:54 +0200

    first commit

---
+++ StringCalcManager/StringCalc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalcManager
+{
+    public class StringCalc
+    {
+        const char delimiter = ',';
+        public static int Add(string numbers)
+        {
+            
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            else
+            {
+                if (numbers.EndsWith("\n"))
+                {
+                    throw new NotSupportedException("Non è possibile terminare con una nuova linea");
+                }
+                else
+                {
+
+                    var stringNumbersArray = numbers.Replace('\n', delimiter)
+                                                    .Split(delimiter);
+
+                    GetNumbersArrayDefaultDelimiter(ref stringNumbersArray);
+                    var numbersArray = stringNumbersArray.Where(x=>!string.IsNullOrEmpty(x))
+                                                         .Select(x => int.Parse(x))
+                                                         .ToArray();
+                    ValidateNonNegatives(numbersArray);
+
+                    return numbersArray.Where(x=>x<1000)
+                                       .Sum(x => x);
+                }
+            }
+        }
+
+        private static void ValidateNonNegatives(int[] numbersArray)
+        {
+            if (numbersArray.Any(x => x < 0))
+                throw new Exception($"negatives not allowed:{string.Join(" ", numbersArray.Where(x => x < 0))}");
+        }
+
+        private static void GetNumbersArrayDefaultDelimiter(ref string[] stringNumbersArray)
+        {
+
+            if (!stringNumbersArray[0].StartsWith("//"))
+                return;
+            
+            var customDelimiters = stringNumbersArray[0].Remove(0, 2).Distinct();
+            foreach (var customDelimiter in customDelimiters)
+            {
+                stringNumbersArray[1] = stringNumbersArray[1].Replace(customDelimiter, delimiter);
+            }
+            //stringNumbersArray[1] = stringNumbersArray[1].Replace(customDelimiter, delimiter.ToString());
+            stringNumbersArray = stringNumbersArray[1].Split(delimiter);
+        }
+    }
+}
---
+++ StringCalcTest/MyStringCalcTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringCalcManager;
+
+
+namespace StringCalcTest
+{
+    [TestFixture]
+    class MyStringCalcTest
+    {
+        [TestCase(0,"")]
+        [TestCase(5, "5")]
+        [TestCase(4, "1,3")]
+        [TestCase(12, "1,4,5,2")]
+        [TestCase(6, "1\n2,3")]
+        [TestCase(3, "//;\n1;2")]
+        [TestCase(2, "//;\n1001;2")]
+        [TestCase(6, "//***\n1***2***3")]
+        [TestCase(6, "//*%\n1*2%3")]
+        [TestCase(6, "//*%%\n1*2%%%%3")]
+        public void Add_Numbers_ReturnSum(int expected, string numbers)
+        {
+            Assert.AreEqual(expected, StringCalc.Add(numbers));
+        }
+
+        [Test]
+        public void Add_NegativesNumbers_ThrowError()
+        {
+            var numbers = "1\n-2,-3";
+            var exeption= Assert.Throws<Exception>(()=> StringCalc.Add(numbers));
+            Assert.AreEqual("negatives not allowed:-2 -3", exeption.Message);
+
+        }
+
+
+    }
+}
